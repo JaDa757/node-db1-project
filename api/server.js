6ea5e59d2e807a9f68f9b70191a33dc8ddf0c5df
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-27 14:26:48 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -4,4 +4,9 @@ const server = express();
 
 server.use(express.json());
 
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: "not found, server.js",
+    })
+})
 module.exports = server;
