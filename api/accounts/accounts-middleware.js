cac306c0c01e3a8499fac4b6883d016e636f0e58
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-27 16:30:58 -0500

    post

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,7 @@
 const Account = require('./accounts-model')
 
+const db = require('../../data/db-config')
+
 exports.checkAccountPayload = (req, res, next) => {
   const error = { status: 400 };
   const { name, budget } = req.body;
@@ -15,13 +17,31 @@ exports.checkAccountPayload = (req, res, next) => {
   } else if (typeof budget !== 'number' || isNaN(budget)) {
     error.message = 'budget of account must be a number'
     next(error)
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
   }
-  next();
+
+  if (error.message) {
+    next(error)
+  } else {
+    next();
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  console.log('check account unique name middleware')
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db('accounts')
+      .where('name', req.body.name.trim())
+      .first()
+
+    if (existing) {
+      next({ status: 400, message: 'that name is taken' })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
